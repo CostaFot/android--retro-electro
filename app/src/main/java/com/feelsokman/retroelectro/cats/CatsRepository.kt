0590b2195d63ff3937f94f56e465d9152087fcf0
Kosta <jarlaxe95@gmail.com>
2019-02-18 00:33:44 +0000

    Hihi

--- app/src/main/java/com/feelsokman/retroelectro/cats/CatsRepository.kt
+++ app/src/main/java/com/feelsokman/retroelectro/cats/CatsRepository.kt
@@ -4,6 +4,9 @@ import com.feelsokman.retroelectro.model.NetCat
 import com.feelsokman.retroelectro.repository.Repository
 import io.reactivex.Single
 
+/**
+ * This guy extends Repository class so the retrofit variable will be available to use as it's instantiated in the init!
+ */
 class CatsRepository(
     baseUrl: String,
     isDebugEnabled: Boolean,
@@ -12,6 +15,7 @@ class CatsRepository(
 
     private val catsDataSource: CatsDataSource = CatsDataSource(retrofit)
 
+    // a class to wrap around the response to make things easier later
     inner class Result(val netCats: List<NetCat>? = null, val errorMessage: String? = null) {
 
         fun hasCats(): Boolean {
@@ -23,6 +27,7 @@ class CatsRepository(
         }
     }
 
+    // the method that's gonna be called by our activity
     fun getNumberOfRandomCats(limit: Int, category_ids: Int?): Single<Result> {
 
         return catsDataSource.getNumberOfRandomCats(limit, category_ids)
